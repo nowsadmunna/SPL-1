ef73c5b3778d6f30cf8b3db6160f25e756d8b5d6
nowsadmunna <bsse1407@iit.du.ac.bd>
2023-09-21 22:06:30 +0600

    trying

--- derive.cpp
+++ derive.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include"header.h"
 using namespace std;
-string differentiate(cahr variaable,int power,int coefficient,char sign)
+string differentiate(char variaable,int power,int coefficient,char sign)
 {
     string answer="";
 }
--- display.cpp
+++ display.cpp
@@ -2,11 +2,11 @@
 #include "header.h"
 using namespace std;
 
-string answer="";
-void as_algebric(string equation)
-{
+// string answer="";
+// void as_algebric(string equation)
+// {
 
-}
+// }
 int check_u_or_uplusv(string equation)
 {
     int plus_or_minus_count=0;
@@ -24,45 +24,45 @@ int check_u_or_uplusv(string equation)
     return plus_or_minus_count;
 }
 
-void display()
-{
-        cout<<"Enter the equation that you want to differentiate"<<endl;
-        cout<<"Option:"<<endl;
-        cout<<"1.Input Equation"<<endl;
-        cout<<"2.Exit"<<endl;
-        cout<<"Enter your choice: ";
-        int choice;
-        cin>>choice;
-        getchar();
-        if(choice==1)
-        {
-            string equation;
-            cout<<"Enter The Equation: ";
-            getline(cin,equation);
-            equation=removespaces(equation);
-            cout<<equation<<endl;
-           cout<<"Answer: ";
-           bool uv_recognizer=check_uv_function(equation);
-           if(uv_recognizer==true)
-           {
-                answer+=as_uv_function(equation);
-           }
-        }
-}
-string removespaces(string equation)
-{
-    string space_removal_string;
-    int i;
-    for(i=0;equation[i]!='\0';i++)
-    {
-        if(equation[i]==' ')
-            {
-                continue;
-            }
-        else
-            {
-                space_removal_string+=equation[i];
-            }
-    }
-    return space_removal_string;
-}
+// void display()
+// {
+//         cout<<"Enter the equation that you want to differentiate"<<endl;
+//         cout<<"Option:"<<endl;
+//         cout<<"1.Input Equation"<<endl;
+//         cout<<"2.Exit"<<endl;
+//         cout<<"Enter your choice: ";
+//         int choice;
+//         cin>>choice;
+//         getchar();
+//         if(choice==1)
+//         {
+//             string equation;
+//             cout<<"Enter The Equation: ";
+//             getline(cin,equation);
+//             equation=removespaces(equation);
+//             cout<<equation<<endl;
+//            cout<<"Answer: ";
+//            bool uv_recognizer=check_uv_function(equation);
+//            if(uv_recognizer==true)
+//            {
+//                 answer+=as_uv_function(equation);
+//            }
+//         }
+// }
+// string removespaces(string equation)
+// {
+//     string space_removal_string;
+//     int i;
+//     for(i=0;equation[i]!='\0';i++)
+//     {
+//         if(equation[i]==' ')
+//             {
+//                 continue;
+//             }
+//         else
+//             {
+//                 space_removal_string+=equation[i];
+//             }
+//     }
+//     return space_removal_string;
+// }
